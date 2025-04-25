840ace147eae4552f35985b27650469f8f75adfe
Julia <juliaangxy8396@gmail.com>
2025-04-25 18:15:06 +0800

    modified:   avatar/interactive/api/function_app.py

--- avatar/interactive/api/function_app.py
+++ avatar/interactive/api/function_app.py
@@ -592,7 +592,7 @@ def get_speech_token(req: Request) -> JSONResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     # Define token endpoint
-    token_endpoint = f"https://southeastasia.api.cognitive.microsoft.com/sts/v1.0/issueToken"
+    token_endpoint = f"https://{region}.api.cognitive.microsoft.com/sts/v1.0/issueToken"
 
     # Make HTTP request with subscription key as header
     # response = requests.post(token_endpoint, headers={"Ocp-Apim-Subscription-Key": subscription_key})
